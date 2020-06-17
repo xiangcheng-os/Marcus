ed7aaa29bfe6da312cbd46e0caa2cd5d11c89afb
xiangcheng <572176737@qq.com>
2020-06-17 18:00:11 +0800

    footer

---
+++ src/components/NavFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="footer">
+    <div class="footer-logo">
+      <img src="" alt="">
+      <p>小米商城</p>
+    </div>
+    <div class="footer-link">
+      <a href="">小米真香</a>
+      <a href="">华为可以</a>
+      <a href="">苹果也行</a>
+      <a href="">三星就算了</a>
+      <a href="">锤子加油</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
