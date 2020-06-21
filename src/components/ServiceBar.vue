18c380286dda42203eb234aa4a62ab934f3a3724
xiangcheng <572176737@qq.com>
2020-06-21 22:53:28 +0800

    数据请求有问题

--- src/components/ServiceBar.vue
+++ src/components/ServiceBar.vue
@@ -3,9 +3,9 @@
     <div class="container">
       <ul>
         <li><span class="icon-setting"></span>预约维修服务</li>
-        <li><span class="icon-setting"></span>七天无理由退货</li>
-        <li><span class="icon-setting"></span>15天免费换货</li>
-        <li><span class="icon-setting"></span>满150元包邮</li>
+        <li><span class="icon-7day"></span>七天无理由退货</li>
+        <li><span class="icon-15day"></span>15天免费换货</li>
+        <li><span class="icon-post"></span>满150元包邮</li>
       </ul>
     </div>
   </div>
@@ -40,14 +40,14 @@ export default {
       .icon-setting{
         @include bgImg(20px,20px,'/imgs/icon-setting.png');
       }
-      .icon-setting{
-        @include bgImg(20px,20px,'/imgs/icon-setting.png');
+      .icon-7day{
+        @include bgImg(20px,20px,'/imgs/icon-7day.png');
       }
-      .icon-setting{
-        @include bgImg(20px,20px,'/imgs/icon-setting.png');
+      .icon-15day{
+        @include bgImg(20px,20px,'/imgs/icon-15day.png');
       }
-      .icon-setting{
-        @include bgImg(20px,20px,'/imgs/icon-setting.png');
+      .icon-post{
+        @include bgImg(20px,20px,'/imgs/icon-post.png');
       }
 
     }
