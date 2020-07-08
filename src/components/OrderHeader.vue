fcac6868f1191285577b285c9139dbc44c962734
mengxiangcheng2019 <572176737@qq.com>
2020-07-08 23:29:35 +0800

    好热

---
+++ src/components/OrderHeader.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="order-header">
+    <div class="container">
+      <div class="header-logo">
+        <a href="/#/index"></a>
+      </div>
+      <div class="title">
+        <h2>我的购物车<span>温馨提示</span></h2>
+      </div>
+      <div class="username">
+        <a href="javascript:;"></a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'order-header'
+
+}
+</script>
+
+<style lang="scss">
+.order-header{
+  padding: 30px 0;
+  .header-logo{
+    float:left;
+  }
+  .title,.username{
+    display: inline-block;
+    height: 55px;
+    line-height: 55px;
+  }
+  .title{
+    font-size: 28px;
+    float: left;
+    span{
+      font-size: 14px;
+      margin-left: 17px;
+      color: #333;
+    }
+  }
+  .username{
+    float: right;
+  }
+}
+
+</style>
---
+++ src/pages/cart.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div>
+      <order-header></order-header>
+    </div>
+    <service-bar></service-bar>
+    
+  </div>
+  
+</template>
+
+<script>
+import Orderheader from './../components/OrderHeader'
+import ServiceBar from './../components/ServiceBar'
+export default {
+  name:'cart',
+  data(){
+
+  },
+
+  components:{
+    Orderheader,
+    ServiceBar
+
+  }
+
+
+}
+</script>
+
+<style>
+
+</style>
